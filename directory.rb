48bd960b81a3c819648fcbf9e423c6f4d6513297
Nikhil Varadharajan <nikkivarad30@gmail.com>
2022-10-24 09:38:05 +0100

    Step 8: Q1-3 Refactor and include all print methods (8.1-8.3)

--- directory.rb
+++ directory.rb
@@ -22,6 +22,31 @@ def input_students
     puts "-------------"
   end
   
+ # 8.1 
+  def print(students)
+    students.each_with_index do |student, index|
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)"
+    end
+  end
+  
+# 8.2
+  def print_by_first_letter(students)
+    puts "Student names beginning with: (Please enter a letter)"
+    letter = gets.chomp
+    number_of_matches = 0
+    students.each do |student|
+      if student[:name].start_with?(letter.upcase, letter.downcase)
+        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+        number_of_matches += 1
+      else
+      end  
+    end
+    puts
+    puts "We have #{number_of_matches} students whose name begins with #{letter}"
+    puts
+  end
+  
+# 8.3
   def print_by_name_length(students)
     puts "Names with maximum characters of: (Please enter a number)"
     max_length = gets.chomp
@@ -42,8 +67,7 @@ def input_students
     puts "Overall, we have #{names.count} great students"
   end
 
-# Nothing happens until we call methods
-
+# Call the methods
 students = input_students
 print_header
 print(students)
