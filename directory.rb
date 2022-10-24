d829b9e483b268e858225901c91c23112d5b4dc8
Nikhil Varadharajan <nikkivarad30@gmail.com>
2022-10-24 11:25:05 +0100

    Step 8: Q7 Update input_students method to get cohort info from user

--- directory.rb
+++ directory.rb
@@ -1,13 +1,16 @@
-# 8.5
+# 8.6 Add method to center strings (8.6)
+@width = 50
+
+# 8.5 Enrich the student information with country and hobbies
 def create_new_student
-    puts "Do you want to create a new student? (y/n)"
+    puts "Do you want to create a new student? (y/n)".center(@width)
     continue = gets.chomp.downcase
     if continue == "y"
       return true
     elsif continue == "n"
       return false
     else
-      puts "Invalid choice"
+      puts "Invalid choice".center(@width)
     end
     create_new_student
   end
@@ -16,14 +19,16 @@ def input_students
     students = []
     continue = create_new_student
     while continue
-      puts "Please enter the name of the student"
-    name = gets.chomp
-    puts "Please enter the country of birth"
-    country_of_birth = gets.chomp
-    hobbies = add_hobbies
-    students << { name: name, cohort: :november, country_of_birth: country_of_birth, hobbies: hobbies }
-    puts "Now we have #{students.count} students"
-    continue = create_new_student
+      puts "Please enter the name of the student".center(@width)
+      name = gets.chomp
+      puts "Please enter the cohort".center(@width)
+      cohort = gets.chomp
+      puts "Please enter the country of birth".center(@width)
+      country_of_birth = gets.chomp
+      hobbies = add_hobbies
+      students << { name: name, cohort: cohort, country_of_birth: country_of_birth, hobbies: hobbies }
+      puts "Now we have #{students.count} students".center(@width)
+      continue = create_new_student
     end
 
     students
@@ -31,8 +36,8 @@ def input_students
   
   def add_hobbies
     hobbies = []
-    puts "Please enter student's hobbies"
-    puts "(To finish, just hit return twice)"
+    puts "Please enter student's hobbies".center(@width)
+    puts "(To finish, just hit return twice)".center(@width)
     hobby = gets.chomp
     while !hobby.empty?
       hobbies << hobby
@@ -42,55 +47,55 @@ def input_students
   end
 
   def print_header
-    puts "The students of Villains Academy"
-    puts "-------------"
+    puts "The students of Villains Academy".center(@width)
+    puts "-------------".center(@width)
   end
   
  # 8.4  - Rewrite print method using a while loop (already did this earlier...)
   def print(students)
     i = 0
     while i < students.count
-    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:country_of_birth]} (#{students[i][:cohort]})"
+    puts "#{i + 1}. #{students[i][:name]}, #{students[i][:country_of_birth]} (#{students[i][:cohort]})".center(@width)
     i += 1
     end
   end
   
 # 8.2
   def print_by_first_letter(students)
-    puts "Student names beginning with: (Please enter a letter)"
+    puts "Student names beginning with: (Please enter a letter)".center(@width)
     letter = gets.chomp
     number_of_matches = 0
     students.each do |student|
       if student[:name].start_with?(letter.upcase, letter.downcase)
-        puts "#{student[:name]}, #{students[i][:country_of_birth]} (#{student[:cohort]} cohort)"
+        puts "#{student[:name]}, #{students[i][:country_of_birth]} (#{student[:cohort]} cohort)".center(@width)
         number_of_matches += 1
       else
       end
     end
     puts
-    puts "We have #{number_of_matches} students whose name begins with #{letter}"
+    puts "We have #{number_of_matches} students whose name begins with #{letter}".center(@width)
     puts
   end
   
 # 8.3
   def print_by_name_length(students)
-    puts "Names with maximum characters of: (Please enter a number)"
+    puts "Names with maximum characters of: (Please enter a number)".center(@width)
     max_length = gets.chomp
     number_of_matches = 0
     students.each do |student|
       if student[:name].length <= max_length.to_i
-        puts "#{student[:name]}, #{students[i][:country_of_birth]} (#{student[:cohort]} cohort)"
+        puts "#{student[:name]}, #{students[i][:country_of_birth]} (#{student[:cohort]} cohort)".center(@width)
         number_of_matches += 1
       else
       end  
     end
     puts
-    puts "We have #{number_of_matches} students with a name of maximum #{max_length} characters"
+    puts "We have #{number_of_matches} students with a name of maximum #{max_length} characters".center(@width)
     puts
   end
 
   def print_footer(names)
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{names.count} great students".center(@width)
   end
 
 # Call the methods
